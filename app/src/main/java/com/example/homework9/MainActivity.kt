5923594613915b3934065d9426ca6e840e16ca89
lmelikishvili <levanimelikishvili@gmail.com>
2023-11-16 11:24:10 +0400

    before_prod

--- app/src/main/java/com/example/homework9/MainActivity.kt
+++ app/src/main/java/com/example/homework9/MainActivity.kt
@@ -2,18 +2,25 @@ package com.example.homework9
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.GridView
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.example.homework9.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var gridView: GridView
     private val products = mutableListOf<Product>()
+    private val storedProducts = mutableListOf<Product>()
     private lateinit var productAdapter: ProductAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setProducts()
+        products.addAll(storedProducts)
 
 
 
@@ -21,18 +28,63 @@ class MainActivity : AppCompatActivity() {
         productAdapter = ProductAdapter(applicationContext, products)
         gridView.adapter = productAdapter
 
-        setProducts()
+
+
+        val btnParty: LinearLayout = findViewById(R.id.catParty)
+        val btnBeach: LinearLayout = findViewById(R.id.catBeach)
+        val btnCamp: LinearLayout = findViewById(R.id.catCamping)
+        val btnAll: TextView = findViewById(R.id.imgAll)
+
+
+        btnParty.setOnClickListener {
+            products.clear()
+            for (p in storedProducts) {
+                if (p.category == "party")
+                    products.add(p)
+            }
+            println(products)
+            productAdapter.notifyDataSetChanged()
+        }
+
+        btnBeach.setOnClickListener {
+            products.clear()
+            for (p in storedProducts) {
+                if (p.category == "beach")
+                    products.add(p)
+            }
+            println(products)
+            productAdapter.notifyDataSetChanged()
+        }
+
+        btnCamp.setOnClickListener {
+            products.clear()
+            for (p in storedProducts) {
+                if (p.category == "camp")
+                    products.add(p)
+            }
+            println(products)
+            productAdapter.notifyDataSetChanged()
+        }
+
+        btnAll.setOnClickListener {
+            products.clear()
+            products.addAll(storedProducts)
+            productAdapter.notifyDataSetChanged()
+        }
+    }
+
+    private fun actionsBtns(){
 
     }
 
     private fun setProducts(){
-        products.also {
+        storedProducts.also {
             it.add(
                 Product(
                     image = R.drawable.camp_11,
                     title = "Belt suit blazer",
                     price = 120,
-                    category = "zamtariaaa"
+                    category = "camp"
 
             ))
             it.add(
@@ -40,7 +92,7 @@ class MainActivity : AppCompatActivity() {
                     image = R.drawable.beach_1,
                     title = "Belt suit blazer",
                     price = 120,
-                    category = "zamtariaaa"
+                    category = "beach"
 
                 ))
             it.add(
@@ -48,7 +100,7 @@ class MainActivity : AppCompatActivity() {
                     image = R.drawable.party_1,
                     title = "Belt suit blazer",
                     price = 120,
-                    category = "zamtariaaa"
+                    category = "party"
 
                 ))
             it.add(
@@ -56,9 +108,43 @@ class MainActivity : AppCompatActivity() {
                     image = R.drawable.camp_2,
                     title = "Belt suit blazer",
                     price = 120,
-                    category = "zamtariaaa"
+                    category = "camp"
+
+                ))
+
+            it.add(
+                Product(
+                    image = R.drawable.camp_11,
+                    title = "Belt suit blazer",
+                    price = 120,
+                    category = "camp"
+
+                ))
+            it.add(
+                Product(
+                    image = R.drawable.beach_1,
+                    title = "Belt suit blazer",
+                    price = 120,
+                    category = "beach"
+
+                ))
+            it.add(
+                Product(
+                    image = R.drawable.party_1,
+                    title = "Belt suit blazer",
+                    price = 120,
+                    category = "party"
+
+                ))
+            it.add(
+                Product(
+                    image = R.drawable.camp_2,
+                    title = "Belt suit blazer",
+                    price = 120,
+                    category = "camp"
 
                 ))
         }
+
     }
 }
